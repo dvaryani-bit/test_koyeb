4383839fb1e7b1745e5cef45df7a2a948cbffa09
Daksh Varyani <77307956+dvaryani-bit@users.noreply.github.com>
2024-01-06 11:04:37 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+@app.route("/")
+def home():
+    #### testing if return render_template works
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    json_file = 'test.json'
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+    app.run(port = 5000)
+    #### testing if opening a file works
